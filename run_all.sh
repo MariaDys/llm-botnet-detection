18e2677dd81c39b14a3afb38e663baf93178a44e
MariaDys <mariadyskina@gmail.com>
2026-05-28 09:46:12 +0300

    Add results for 9 devices, baseline, run_all.sh

---
+++ run_all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cd ~/Desktop/botnet
+
+declare -A devices
+# devices[1]="device_1_danmini_doorbell" уже прогнали вручную -- раскомментировать, если нужно прогнать снова и добавить ниже в цикл for device in 1 2 3 ...
+devices[2]="device_2_ecobee_thermostat"
+devices[3]="device_3_ennio_doorbell"
+devices[4]="device_4_philips_baby_monitor"
+devices[5]="device_5_provision_pt737e"
+devices[6]="device_6_provision_pt838"
+devices[7]="device_7_samsung_webcam"
+devices[8]="device_8_simplehome_1002"
+devices[9]="device_9_simplehome_1003"
+
+for device in 2 3 4 5 6 7 8 9; do
+    folder="results/${devices[$device]}"
+    echo "========== ${devices[$device]} =========="
+    mkdir -p "$folder"
+
+    for script in claude_en claude_ru deepseek_en deepseek_ru gigachat_en gigachat_ru; do
+        echo "--- Running ${script}.py on device $device ---"
+        python3 generated_code/${script}.py --data_dir dataset --device_id $device \
+            2>&1 | tee "${folder}/${script}.txt"
+    done
+done
+
+echo "DONE!"
